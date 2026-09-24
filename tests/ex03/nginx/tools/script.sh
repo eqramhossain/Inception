273eb1255e424219992291ed320322251535db2e
ehossain_debianVM <ekram.fr03+ehossaindebianVM@gmail.com>
2026-09-24 12:19:42 +0200

    There was a lot of error in the script.sh i was executing all cmd with exec
    when we execute a cmd using exec it replace the process with the current
    cmd pid and it became the pid1. so when the cd cmd finishes it works the
    container life end and it exit

--- tests/ex03/nginx/tools/script.sh
+++ tests/ex03/nginx/tools/script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cd /etc/nginx/ && mkdir -p ssl && cd ssl
+
+openssl req -x509 -newkey rsa:4096 -sha256 -noenc -keyout server.key -out server.crt -days 3650 -subj "/C=FR/ST=Paris/L=Paris/O=42paris/OU=Inception/CN=ehossain.42.fr"
+
 exec nginx -g "daemon off;"
