56167c45a70b9519543a4e071c5c21d0e31b8c61
ehossain_debianVM <ekram.fr03+ehossaindebianVM@gmail.com>
2026-09-24 18:58:44 +0200

    done nginx part

---
+++ srcs/requirements/ngnix/tools/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /etc/nginx/ && mkdir -p ssl && cd ssl
+
+openssl req -x509 -newkey rsa:4096 -sha256 -keyout server.key -out server.crt -days 365 -noenc -subj "/C=FR/ST=Paris/L=Paris/O=42paris/OU=Inception/CN=ehossain.42.fr"
+
+exec "$@"
